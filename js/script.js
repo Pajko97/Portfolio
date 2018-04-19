128cbfb362c66c8dbd1081b2b817f9bf733fd6ba
Pavle Janjusevic <axeofthedead@gmail.com>
2018-04-19 17:45:49 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready( function() {
+    $('#home-btn').click(function() {
+        $('li').removeClass('current');
+        $('#home-btn').addClass('current');
+        $("#content").hide().load('/includes/home.html').fadeIn('slow');
+
+        return false;
+    });
+    
+    $('#work-btn').click(function() {
+        $('li').removeClass('current');
+        $('#work-btn').addClass('current');
+        $("#content").hide().load('/includes/work.html').fadeIn('slow');
+
+        return false;
+    });
+
+    $('#about-btn').click(function() {
+        $('li').removeClass('current');
+        $('#about-btn').addClass('current');
+        $("#content").hide().load('/includes/about.html').fadeIn('slow');
+
+        return false;
+    });
+
+    $('#contact-btn').click(function() {
+        $('li').removeClass('current');
+        $('#contact-btn').addClass('current');
+        $("#content").hide().load('/includes/contact.html').fadeIn('slow');
+
+        return false;
+    });
+
+    
+});
+
+
