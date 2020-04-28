60005eafeed6b8ba4ae5648d6361634d1d1b142a
Pavle Janjusevic <devredhat@gmail.com>
2020-04-28 04:52:15 +0200

    new portfolio

--- js/main.js
+++ js/main.js
@@ -1,50 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $('.link-about').click(function () {
+        $('.nav-link').removeClass('nav-link-active');
+        $('.nav-link-about').addClass('nav-link-active');
+        $(".content").hide().load('/includes/About.html').fadeIn('slow');
 
-    var config = {
-        apiKey: "AIzaSyCinEYD4VoLNLJpqnO3HcbU_rPTczL9ZPY",
-        authDomain: "portfolio-c68df.firebaseapp.com",
-        databaseURL: "https://portfolio-c68df.firebaseio.com",
-        projectId: "portfolio-c68df",
-        storageBucket: "portfolio-c68df.appspot.com",
-        messagingSenderId: "607710327979"
-      };
+        return false;
+    });
 
-      if (!firebase.apps.length) {
-        firebase.initializeApp(config);
-     }
+    $('.link-work').click(function () {
+        $('.nav-link').removeClass('nav-link-active');
+        $('.nav-link-work').addClass('nav-link-active');
+        $(".content").hide().load('/includes/Work.html').fadeIn('slow');
 
-      var messagesRef = firebase.database().ref('messages');
-
-document.getElementById("contactForm").addEventListener('submit', sendForm);
-
-function getInputVal(id) {
-    return document.getElementById(id).value;
-}
+        return false;
+    });
 
 
-function sendForm(e) {
-    e.preventDefault();
 
-    var name= getInputVal('name-field');
-    var mail = getInputVal('mail-field');
-    var message = getInputVal('message-field');
-
-    saveMessage(name, mail, message);
-
-    document.querySelector('#contactForm').style.display = 'none';
-    document.querySelector('.header').style.display = 'none';
-    document.querySelector('.sent').style.display = 'block';
-
-  }
-
-  function saveMessage(name, mail, message) {
-      let newMessageRef = messagesRef.push();
-      newMessageRef.set({
-          name: name,
-          email: mail,
-          message: message
-      })
-  }
-
-
-})
+});
